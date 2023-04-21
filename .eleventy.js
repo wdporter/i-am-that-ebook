d0f23201c2b2321d4d32244adaa0312efcfb7102
David Porter <david.porter@fastmail.com>
2023-04-22 08:58:00 +1000

    fix errors

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = config => {
   config.addPassthroughCopy("src/META-INF")
   config.addPassthroughCopy("src/package.opf")
   config.addPassthroughCopy("src/mimetype")
-  config.addPassthroughCopy("src/toc.ncx")
+  config.addPassthroughCopy("src/toc.xhtml")
 
   return {
 
