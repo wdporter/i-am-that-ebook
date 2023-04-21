b30c030364088db96339a58f90e2d2457bdd0973
David Porter <david.porter@fastmail.com>
2023-04-22 09:22:56 +1000

    add cover

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = config => {
   config.addPassthroughCopy("src/package.opf")
   config.addPassthroughCopy("src/mimetype")
   config.addPassthroughCopy("src/toc.xhtml")
+  config.addPassthroughCopy("src/cover.jpg")
 
   return {
 
